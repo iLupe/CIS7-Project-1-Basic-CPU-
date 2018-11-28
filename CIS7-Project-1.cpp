e6ab2ff8e95f17134a867f803d0c9ca600a8dc8d
JonathanC1021 <38872639+JonathanC1021@users.noreply.github.com>
2018-11-27 18:51:08 -0800

    Create CIS7-Project-1.cpp

---
+++ CIS7-Project-1.cpp
@@ -0,0 +1,56 @@
+// Jonathan Carreon
+// CIS7
+// Project: Case Project 3 (Encryption)
+// Team Name: Basic CPU
+#include <iostream>
+using namespace std;
+
+string generateKey(string text, string key)
+{
+  int v = text.size();
+
+  for (int i = 0; ; i++)
+  {
+    if (v == i)
+      i = 0;
+    if (key.size() == text.size())
+      break;
+    key.push_back(key[i]);
+  }
+  return key; 
+}
+
+string cipherText(string text, string key)
+{
+  string cipher_text;
+
+  for (int i = 0; i < text.size(); i++)
+  {
+    int v = (text[i] + key[i]) %26;
+
+    v += 'A';
+
+    cipher_text.push_back(v);
+  }
+  return cipher_text;
+}
+
+// Use 'WEWILLTRY' as Plaintext
+// Use 'WOW' as keyword
+int main()
+{
+  string text, keyWord;
+
+  cout << "Insert Plaintext: ";
+  cin >> text;
+
+  cout << "Insert Keyword: ";
+  cin >> keyWord;
+
+  string key = generateKey(text, keyWord);
+  string cipher_text = cipherText(text, key);
+
+  cout << "Ciphertext: " << cipher_text << "\n";
+
+  return 0;
+}
